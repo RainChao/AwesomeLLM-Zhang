ed9339305e47cdf5ceb11bb8197b7e9af7bd44df
stoneren <stoneren@tencent.com>
2024-06-02 17:01:23 +0800

    add ddp

--- data_set.py
+++ data_set.py
@@ -1,35 +1,50 @@
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
+import torch.distributed as dist
 import os
 import requests
 import tiktoken
 import torch
+import Random
 
-
-# Load training data
-if not os.path.exists('data/sales_textbook.txt'):
-    url = 'https://huggingface.co/datasets/goendalf666/sales-textbook_for_convincing_and_selling/raw/main/sales_textbook.txt'
-    with open('data/sales_textbook.txt', 'w') as f:
-        f.write(requests.get(url).text)
-
-with open('data/sales_textbook.txt', 'r', encoding='utf-8') as f:
-    text = f.read()
 
 # Using TikToken (Same as GPT3) to tokenize the source text
-encoding = tiktoken.get_encoding("cl100k_base")
-tokenized_text = encoding.encode(text)
-# the maximum value of the tokenized numbers
-max_token_value = max(tokenized_text) + 1
-# put tokenized text into tensor
-tokenized_text = torch.tensor(tokenized_text, dtype=torch.long)
+local_tokenizer = None
 
-# Split train and validation
-split_idx = int(len(tokenized_text) * 0.9)
-train_data = tokenized_text[:split_idx]
-val_data = tokenized_text[split_idx:]
+
+def get_local_tokenizer():
+    """Get global tokenizer for this project"""
+    global local_tokenizer
+    if local_tokenizer is None:
+        local_tokenizer = tiktoken.get_encoding("cl100k_base")
+    return local_tokenizer
+
+
+def get_text_data():
+    """Load training data"""
+    if not os.path.exists('data/sales_textbook.txt'):
+        url = 'https://huggingface.co/datasets/goendalf666/sales-textbook_for_convincing_and_selling/raw/main/sales_textbook.txt'
+        with open('data/sales_textbook.txt', 'w') as f:
+            f.write(requests.get(url, timeout=60).text)
+
+    with open('data/sales_textbook.txt', 'r', encoding='utf-8') as f:
+        text = f.read()
+
+    tokenized_text = get_local_tokenizer().encode(text)
+    # the maximum value of the tokenized numbers
+    max_token_value = max(tokenized_text) + 1
+    # put tokenized text into tensor
+    tokenized_text = torch.tensor(tokenized_text, dtype=torch.long)
+
+    # Split train and validation
+    split_idx = int(len(tokenized_text) * 0.9)
+    train_data = tokenized_text[:split_idx]
+    val_data = tokenized_text[split_idx:]
+    return train_data, val_data, max_token_value
 
 
 class SequenceBatch(Dataset):
+    """"Generate user defined dataset for DataParallel Training Mode"""
     def __init__(self, data, context_length):
         self.data = data
         self.context_length = context_length
@@ -43,7 +58,9 @@ class SequenceBatch(Dataset):
 
 
 def get_dataloader(batch_size, context_length):
+    """"Generate train and valuate data for DataParallel Training Mode"""
     torch.manual_seed(123)
+    train_data, val_data, max_token_value = get_text_data()
     train_dataset = SequenceBatch(train_data, context_length)
     train_loader = DataLoader(
         dataset=train_dataset,
@@ -62,8 +79,74 @@ def get_dataloader(batch_size, context_length):
 
 
 def text_to_ids(input_text):
-    return encoding.encode(input_text)
+    """Converts a string to a list of ids"""
+    return get_local_tokenizer().encode(input_text)
 
 
 def idx_to_text(input_ids):
-    return encoding.decode(input_ids)
+    """Converts a list of ids to a string"""
+    return get_local_tokenizer().decode(input_ids)
+
+
+class Partitoner(Dataset):
+    """ds = Partitoner(data, indexes, context_length)"""
+
+    def __init__(self, data, indexes, context_length):
+        self.data = data
+        self.indexes = indexes
+        self.context_length = context_length
+
+    def __getitem__(self, index):
+        offset = self.indexes[index]
+        return (self.data[offset:offset + self.context_length],
+                self.data[offset+1:offset + self.context_length + 1])
+
+    def __len__(self):
+        return len(self.indexes)
+
+
+class DataPartitioner(object):
+    """dst = DataPartitioner(data, partition_sizes, context_length, seed=1234)"""
+
+    def __init__(self, data, partition_sizes, context_length, seed=1234):
+        self.data = data
+        self.partition_sizes = partition_sizes
+        self.context_length = context_length
+        indexes = [index for index in range(len(data) - self.context_length)]
+        rng = Random(seed)
+        rng.seed(seed)
+        rng.shuffle(indexes)
+        self.partition_index = []
+        for size in partition_sizes:
+            index = int(size*(len(data) - self.context_length))
+            self.partition_index.append(indexes[:index])
+            indexes = indexes[index:]
+
+    def use(self, partiton: int):
+        """default: use all data"""
+        return Partitoner(self.data, self.partition_index[partiton], self.context_length)
+
+
+def partition_dataset(batch_size, context_length):
+    """get partitioned data for ddp mode"""
+    train_data, eval_dataset, max_token_value = get_text_data()
+    world_size = dist.get_world_size()
+    partition_sizes = [1.0 / world_size for _ in range(world_size)]
+    part_traindata = DataPartitioner(
+        train_data, partition_sizes, context_length)
+    part_train_dataset = part_traindata.use(partiton=dist.get_rank())
+
+    mini_batch_size = batch_size // dist.get_world_size()
+    train_loader = DataLoader(
+        dataset=part_train_dataset,
+        batch_size=mini_batch_size,
+        shuffle=False,
+        drop_last=True,
+    )
+    eval_loader = DataLoader(
+        dataset=eval_dataset,
+        batch_size=mini_batch_size,
+        shuffle=False,
+        drop_last=True,
+    )
+    return train_loader, eval_loader, max_token_value
---
+++ distribute_data_parallel.py
@@ -0,0 +1,87 @@
+import os
+import torch
+import torch.distributed as dist
+from torch.multiprocessing import Process
+from model import TransformerLanguageModel
+from data_set import *
+
+
+# Hyperparameters
+epoch = 1
+batch_size = 128  # How many batches per training step
+# Length of the token chunk each batch
+context_length = 64
+learning_rate = 1e-3  # 0.001
+eval_iters = 20  # Number of iterations to average for evaluation
+# Use GPU if it's available.
+TORCH_SEED = 1337
+torch.manual_seed(TORCH_SEED)
+
+
+def average_gradients(model):
+    """Average gradients across processes."""
+    size = float(dist.get_world_size())
+    for param in model.parameters():
+        if isinstance(param, torch.Tensor):
+            dist.all_reduce(param.grad.data, op=dist.reduce_op.SUM)
+            param.grad.data /= size
+
+
+@torch.no_grad()
+def evaluate_loss(model, data_set, device):
+    """Evaluate loss on a data set."""
+    losses = []
+    model.eval()
+    for x_batch, y_batch in data_set:
+        x_batch = x_batch.to(device)
+        y_batch = y_batch.to(device)
+        _, loss = model(x_batch, y_batch)
+        losses.append(loss.item())
+    model.train()
+    return torch.mean(torch.tensor(losses))
+
+
+def train_loop(local_rank):
+    """Main train loop for one data parallel process."""
+    device = torch.device(local_rank)
+    train_loader, eval_loader, max_token_value = partition_dataset(
+        batch_size, context_length)
+    model = TransformerLanguageModel(context_length=context_length,
+                                     max_token_value=max_token_value, device=device)
+    model.to(device)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
+    for _ in range(epoch):
+        step = 0
+        for x_batch, y_batch in train_loader:
+            x_batch = x_batch.to(device)
+            y_batch = y_batch.to(device)
+            optimizer.zero_grad()
+            _, loss = model(x_batch, y_batch)
+            loss.backward()
+            average_gradients(model)
+            optimizer.step()
+            step += 1
+            if (0 == local_rank) and (step % eval_iters == 0):
+                eval_loss = evaluate_loss(model, eval_loader, device)
+                print('Step:', step, 'Evaluate Loss:', round(
+                    eval_loss.item(), 3), 'Train Loss:', round(loss.item(), 3))
+
+
+def init_processes(local_rank, world_size, fn, backend='nccl'):
+    """ Initialize the distributed environment. """
+    os.environ['MASTER_ADDR'] = '127.0.0.1'
+    os.environ['MASTER_PORT'] = '29500'
+    dist.init_process_group(backend, rank=local_rank, world_size=world_size)
+    fn(local_rank)
+
+
+if __name__ == "__main__":
+    WORLD_SIZE = 2
+    processes = []
+    for rank in range(WORLD_SIZE):
+        p = Process(target=init_processes, args=(rank, WORLD_SIZE, train_loop))
+        p.start()
+        processes.append(p)
+
+    for p in processes:
+        p.join()
